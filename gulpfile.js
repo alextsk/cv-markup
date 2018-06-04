e1c064e677ff24c281a82697dd21a8f609b59abe
Alex <user@nomail.com>
2018-06-04 13:58:34 +0700

    added browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,14 @@ var stylus = require('gulp-stylus');
 //var minifyCSS = require('gulp-csso');
 //var concat = require('gulp-concat');
 //var sourcemaps = require('gulp-sourcemaps');
-
 var deploy      = require('gulp-gh-pages');
+var browserSync = require('browser-sync').create();
 
 gulp.task('html', function(){
   return gulp.src('templates/*.pug')
     .pipe(pug())
-    .pipe(gulp.dest('dist/html'))
+    .pipe(gulp.dest('dist/'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('css', function(){
@@ -18,11 +19,33 @@ gulp.task('css', function(){
     .pipe(stylus())
 //    .pipe(minifyCSS())
     .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('default', [ 'html', 'css']);
+gulp.task('build', [ 'html', 'css']);
 
-gulp.task('deploy', function () {
+gulp.task('deploy', ['build'], function () {
   return gulp.src("./dist/**/*")
     .pipe(deploy())
 });
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('serve', ['html', 'css'], function() {
+
+    browserSync.init({
+        server: "./dist"
+    });
+
+    gulp.watch("templates/**/*.styl", ['css']);
+    gulp.watch("templates/**.pug", ['html'])
+    gulp.watch("dist/**/*.*").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['serve'])
